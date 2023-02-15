46cf028498641e92c7633e3b931dc6671620d5aa
H goto <510hidepon@gmail.com>
2023-02-15 19:25:41 +0900

    Hello World test

--- indexAutorun/Program.cs
+++ indexAutorun/Program.cs
@@ -10,6 +10,8 @@ namespace indexAutorun
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
